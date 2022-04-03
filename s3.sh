9125158eb4d378313d4d09834a4db484e0312701
Adrià Sabaté De la iglesia <15583899.clot@fje.edu>
2022-04-03 18:57:14 +0200

    Pràctica m06uf2pr3

--- s3.sh
+++ s3.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+clear
+
+function compte {
+	local fitxer
+	local ncompt
+	local proba
+
+	ncompt=0
+	for fitxer in $1/*
+	do
+		if [[ -s $fitxer ]] && [[ -x $fitxer ]]
+		then
+			((ncompt++))
+		fi
+	done
+	echo "Numero de fitxers de lectura i més grans que 0: "$ncompt
+	return 0
+}
+
+echo -n "Escriu el directori: "
+read nom
+compte $nom
+echo "Finalitzat"
+exit 0
